3ec1a9d967f0dbab5953083341eaf4fdfb0ee6fb
sigibambarbiev <sigibambarbiev@gmail.com>
2024-01-09 12:45:14 +0200

    check

--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -6,15 +6,12 @@ function Card({ item }) {
 
     return (
         <div style={{marginBottom: "5px"}}>
-            <div>Title: {item.title ? item.title : "title undefined"}</div>
-            <div>text: {item.text ? item.text : "text undefined"}</div>
-            <div>price: {item.price ? item.price : "price undefined"}</div>
-            {/* <div>link: {item.link ? item.link : "link undefined"}</div> */}
+            <div><span style={{color: "#e70c1e", fontWeight: "600"}}>Title: </span>{item.title ? item.title : "title undefined"}</div>
+            <div><span style={{color: "#e70c1e", fontWeight: "600"}}>Text: </span>{item.text ? item.text : "text undefined"}</div>
+            <div><span style={{color: "#e70c1e", fontWeight: "600"}}>price: </span>{item.price ? item.price : "price undefined"}</div>
             <div>
                 <img style={{ width: '200px', height: "120px" }} src={item.link || noFotoProduct} alt="Image" />
             </div>
-            {/* <div>latitude: {item.latitude ? item.latitude : "latitude undefined"}</div> */}
-            {/* <div>longitude: {item.longitude ? item.longitude : "longitude undefined"}</div> */}
         </div>
     );
 }
